0dba31b56f291d43ee0f85356090e7ac63c03a0e
MAIN\Evheniy Parakhonka <por-evgeniy@mail.ru>
2017-10-02 22:24:32 +0300

    added change to conflict

--- app/src/main/java/piftik/github/com/experementsgithub/MainActivity.java
+++ app/src/main/java/piftik/github/com/experementsgithub/MainActivity.java
@@ -1,14 +1,15 @@
 package piftik.github.com.experementsgithub;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 //        first change
+//        change to conflict
     }
 }
