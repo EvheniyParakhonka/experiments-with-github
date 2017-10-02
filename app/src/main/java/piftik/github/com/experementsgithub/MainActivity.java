f83cdc949a7137721d0539d10dba2a6f41a358d5
MAIN\Evheniy Parakhonka <por-evgeniy@mail.ru>
2017-10-02 22:27:19 +0300

    added method to conflict

--- app/src/main/java/piftik/github/com/experementsgithub/MainActivity.java
+++ app/src/main/java/piftik/github/com/experementsgithub/MainActivity.java
@@ -1,7 +1,7 @@
 package piftik.github.com.experementsgithub;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +9,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        first change
+    //        first change
+    }
+    //        create method to conflict
+    public void doSmthng(){
+        
     }
 }
