a9a7e176bbc1bc326c851ce7fe76a508f6449f06
MAIN\Evheniy Parakhonka <por-evgeniy@mail.ru>
2017-10-03 15:40:07 +0300

    added String to rebase, second attempt

--- app/src/main/java/piftik/github/com/experementsgithub/MainActivity.java
+++ app/src/main/java/piftik/github/com/experementsgithub/MainActivity.java
@@ -1,7 +1,7 @@
 package piftik.github.com.experementsgithub;
 
 import android.os.Bundle;
-import android.support.v7.app.*;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,10 +9,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imInProcess();
 //       Rebase
 //       rebase second
     }
-    public void imInProcess(){
-        
+
+    public void imInProcess() {
+        String rebaseSecond = "Second Rebase";
     }
 }
