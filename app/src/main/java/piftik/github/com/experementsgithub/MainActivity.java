628d158adce53868865b36eec8768d3aed79053b
MAIN\Evheniy Parakhonka <por-evgeniy@mail.ru>
2017-10-03 16:15:54 +0300

    added comments

--- app/src/main/java/piftik/github/com/experementsgithub/MainActivity.java
+++ app/src/main/java/piftik/github/com/experementsgithub/MainActivity.java
@@ -14,7 +14,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         mTextView = (TextView) findViewById(R.id.textView);
         imInProcess();
-
     }
 
     public void imInProcess() {
