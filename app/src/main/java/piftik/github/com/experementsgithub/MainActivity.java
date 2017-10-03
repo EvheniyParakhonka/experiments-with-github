6470754d1dcf4fa68b5c2c218c679318cd22edda
MAIN\Evheniy Parakhonka <por-evgeniy@mail.ru>
2017-10-03 15:44:32 +0300

    use method and UI

--- app/src/main/java/piftik/github/com/experementsgithub/MainActivity.java
+++ app/src/main/java/piftik/github/com/experementsgithub/MainActivity.java
@@ -2,17 +2,23 @@ package piftik.github.com.experementsgithub;
 
 import android.os.Bundle;
 import android.support.v7.app.*;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextView;
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mTextView = (TextView) findViewById(R.id.textView);
+        imInProcess();
 //       Rebase
 //       rebase second
     }
     public void imInProcess(){
         String rebase = "Second";
+        mTextView.setText(rebase);
     }
 }
