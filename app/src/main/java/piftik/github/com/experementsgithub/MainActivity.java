cce93b746e6de01fefb32b71d3c839edb855d7a3
MAIN\Evheniy Parakhonka <por-evgeniy@mail.ru>
2017-10-03 15:52:17 +0300

    change import

--- app/src/main/java/piftik/github/com/experementsgithub/MainActivity.java
+++ app/src/main/java/piftik/github/com/experementsgithub/MainActivity.java
@@ -1,7 +1,7 @@
 package piftik.github.com.experementsgithub;
 
 import android.os.Bundle;
-import android.support.v7.app.*;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
